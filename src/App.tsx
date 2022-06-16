e8408652b406130969135b3ba31528ec64c10034
renakaagusta <renakaagusta28@gmail.com>
2022-06-16 08:47:06 +0700

    Remove react helmet

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { LinearProgress } from "@mui/material"
 import { Fragment, lazy, Suspense } from "react"
-import { Helmet } from "react-helmet"
 import { BrowserRouter, Route, Switch } from "react-router-dom"
 
 const Main = lazy(() => import('./pages/Main'))
@@ -8,9 +7,6 @@ const Main = lazy(() => import('./pages/Main'))
 export default function App() {
   return (
     <Fragment>
-      <Helmet>
-        <title>Replasc</title>
-      </Helmet>
       <BrowserRouter>
         <Switch>
           <Route path="/">
